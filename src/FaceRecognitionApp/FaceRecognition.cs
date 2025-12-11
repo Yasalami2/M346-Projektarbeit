74ef814cfe5d473863cb1429825ed120f3c37ca1
Alessandro <alessandro.renzetti894@gmail.com>
2025-12-11 15:08:13 +0100

    Add basic C# FaceRecognition.cs for face recognition

---
+++ src/FaceRecognitionApp/FaceRecognition.cs
@@ -0,0 +1,24 @@
+using System;
+ 
+namespace FaceRecognitionApp
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("FaceRecognitionApp startet...");
+
+
+        }
+
+    }
+
+}
+
+ 
