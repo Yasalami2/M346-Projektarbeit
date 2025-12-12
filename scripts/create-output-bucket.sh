aa71a7aee99ed12ad24a4bb478444207eccbefc8
Alessandro <alessandro.renzetti894@gmail.com>
2025-12-12 14:22:18 +0100

    Implement output S3 bucket creation script

---
+++ scripts/create-output-bucket.sh
@@ -0,0 +1,19 @@
+set -e
+
+REGION="eu-central-1"
+
+OUTPUT_BUCKET="m346-fr-output-facerec-2025"
+ 
+echo "Region:        $REGION"
+echo "Output-Bucket: $OUTPUT_BUCKET"
+echo "--------------------------------------"
+ 
+echo "Erstelle OUTPUT-Bucket..."
+aws s3 mb "s3://$OUTPUT_BUCKET" --region "$REGION"
+ 
+echo
+echo "Buckets mit 'm346' im Namen:"
+aws s3 ls | grep m346 || true
+ 
+echo
+echo "Fertig: Output-Bucket-Script durchgelaufen."
