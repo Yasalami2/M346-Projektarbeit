ad419b1c0f06fbd96c1aa7b4080b64c8f3059f6f
Lionel0Davatz <lionel.davatz@gmx.ch>
2025-12-14 23:31:02 +0100

    Add basic C# Lambda Function skeleton for S3 trigger

--- src/FaceRecognitionApp/FaceRecognition.cs
+++ src/FaceRecognitionApp/FaceRecognition.cs
@@ -1,24 +1,28 @@
 using System;
- 
-namespace FaceRecognitionApp
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
 
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+ 
+namespace FaceRecognitionLambda
 {
-
-    internal class Program
-
+    public class FaceRecognitionFunction
     {
-
-        static void Main(string[] args)
-
+        public async Task FunctionHandler(S3Event evnt, ILambdaContext context)
         {
+            foreach (var record in evnt.Records)
+            {
+                var s3 = record.S3;
+                var inputBucket = s3.Bucket.Name;
+                var key = s3.Object.Key;
 
-            Console.WriteLine("FaceRecognitionApp startet...");
-
-
-        }
-
-    }
-
-}
-
+                context.Logger.LogLine("Neues Objekt im Input-Bucket erkannt:");
+                context.Logger.LogLine($"Bucket: {inputBucket}");
+                context.Logger.LogLine($"Key:    {key}");
+            }
  
+            await Task.CompletedTask;
+        }
+    }
+}
