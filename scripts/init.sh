411a32a03bc34cbd2853fd4f024d1bd228efbbcb
Gabriel Sarkis <gabriel.sarkis@edu.gbssg.ch>
2025-12-15 15:57:07 +0100

    init for buckets

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash
- 
-echo "Init-Script fuer M346 FaceRecognition wird spaeter hier implementiert."
+set -euo pipefail
 
-# TODO: spaeter S3-Buckets erstellen, Lambda deployen, etc.
+export REGION="${REGION:-us-east-1}"
+export INPUT_BUCKET="${INPUT_BUCKET:-m346-fr-input-facerec-2025}"
+export OUTPUT_BUCKET="${OUTPUT_BUCKET:-m346-fr-output-facerec-2025}"
 
- 
+echo "Init FaceRecognition | REGION=$REGION"
+bash ./scripts/create-input-bucket.sh
+bash ./scripts/create-output-bucket.sh
+
+echo
+echo "Buckets bereit:"
+echo "  Input : s3://$INPUT_BUCKET"
+echo "  Output: s3://$OUTPUT_BUCKET"
