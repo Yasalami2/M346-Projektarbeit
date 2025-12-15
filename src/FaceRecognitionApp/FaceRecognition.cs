33dda30138a4f3c0d2dfbc3f0b8a855afa2e7ffa
Gabriel Sarkis <gabriel.sarkis@edu.gbssg.ch>
2025-12-15 09:56:30 +0100

    Add Rekognition logic

--- src/FaceRecognitionApp/FaceRecognition.cs
+++ src/FaceRecognitionApp/FaceRecognition.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.S3Events;
 using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
 using Amazon.S3;
+using Amazon.S3.Model;
 
-// Lambda soll JSON mit SystemTextJson deserialisieren
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace FaceRecognitionLambda
@@ -19,8 +21,8 @@ namespace FaceRecognitionLambda
 
         public FaceRecognitionFunction()
         {
-            _rekognitionClient = new Amazon.Rekognition.AmazonRekognitionClient();
-            _s3Client = new Amazon.S3.AmazonS3Client();
+            _rekognitionClient = new AmazonRekognitionClient();
+            _s3Client = new AmazonS3Client();
 
             _outputBucketName = Environment.GetEnvironmentVariable("OUTPUT_BUCKET")
                 ?? throw new Exception("Environment variable OUTPUT_BUCKET is not set.");
@@ -28,7 +30,7 @@ namespace FaceRecognitionLambda
 
         public async Task FunctionHandler(S3Event evnt, ILambdaContext context)
         {
-            context.Logger.LogLine($"OUTPUT_BUCKET (aus Env): {_outputBucketName}");
+            context.Logger.LogLine($"OUTPUT_BUCKET (Env): {_outputBucketName}");
 
             foreach (var record in evnt.Records)
             {
@@ -39,9 +41,52 @@ namespace FaceRecognitionLambda
                 context.Logger.LogLine("Neues Objekt im Input-Bucket erkannt:");
                 context.Logger.LogLine($"Input-Bucket: {inputBucket}");
                 context.Logger.LogLine($"Key:          {key}");
-            }
 
-            await Task.CompletedTask;
+                var request = new RecognizeCelebritiesRequest
+                {
+                    Image = new Image
+                    {
+                        S3Object = new Amazon.Rekognition.Model.S3Object
+                        {
+                            Bucket = inputBucket,
+                            Name = key
+                        }
+                    }
+                };
+
+                var response = await _rekognitionClient.RecognizeCelebritiesAsync(request);
+
+                var result = new
+                {
+                    InputBucket = inputBucket,
+                    InputKey = key,
+                    Celebrities = response.CelebrityFaces,   
+                    UnrecognizedFaces = response.UnrecognizedFaces.Count,
+                    Timestamp = DateTime.UtcNow
+                };
+
+                var json = JsonSerializer.Serialize(
+                    result,
+                    new JsonSerializerOptions { WriteIndented = true }
+                );
+
+                var outputKey = key + ".json";
+
+                context.Logger.LogLine($"Schreibe Ergebnis nach Output-Bucket:");
+                context.Logger.LogLine($"Output-Bucket:  {_outputBucketName}");
+                context.Logger.LogLine($"Output-Key:     {outputKey}");
+
+                var putRequest = new PutObjectRequest
+                {
+                    BucketName = _outputBucketName,
+                    Key = outputKey,
+                    ContentBody = json
+                };
+
+                await _s3Client.PutObjectAsync(putRequest);
+
+                context.Logger.LogLine("Ergebnis-JSON erfolgreich geschrieben.");
+            }
         }
     }
 }
